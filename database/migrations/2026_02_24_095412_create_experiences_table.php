4f04f0c924c1de2c1b786c0c79300e382d18d858
TAPI MONDAL <tapimondal333@gmail.com>
2026-03-05 16:20:26 +0530

    config,database
    config and database have uploaded

---
+++ database/migrations/2026_02_24_095412_create_experiences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('company');
+            $table->string('location')->nullable();
+            $table->string('position');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->text('description')->nullable();
+            $table->string('reason_to_leave')->nullable(); // file path
+            $table->string('experience_certificate')->nullable(); // file path
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
